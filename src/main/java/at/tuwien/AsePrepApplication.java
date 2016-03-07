c1e95ef8351e21e9a35243a349d6b97272e1338b
geoHeil <e1225063@student.tuwien.ac.at>
2016-03-07 18:22:46 +0100

    use external api url

--- src/main/java/at/tuwien/AsePrepApplication.java
+++ src/main/java/at/tuwien/AsePrepApplication.java
@@ -3,6 +3,7 @@ package at.tuwien;
 import at.tuwien.domain.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,14 +14,18 @@ public class AsePrepApplication implements CommandLineRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(AsePrepApplication.class);
 
+    @Value("${spring.apiUrl}")
+    private String apiUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(AsePrepApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        LOG.info(apiUrl);
         RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        Quote quote = restTemplate.getForObject(apiUrl, Quote.class);
         LOG.info(quote.toString());
     }
 }
