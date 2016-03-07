ceb97a93cc4ea2b3d6c780835f01751c32479adc
geoHeil <e1225063@student.tuwien.ac.at>
2016-03-07 18:03:03 +0100

    add basic service imply stub

---
+++ src/main/java/at/tuwien/domain/services/JokeService.java
@@ -0,0 +1,7 @@
+package at.tuwien.domain.services;
+
+/**
+ * Created by geoHeil on 07.03.16.
+ */
+public interface JokeService {
+}
---
+++ src/main/java/at/tuwien/domain/services/iml/JokeServiceImpl.java
@@ -0,0 +1,7 @@
+package at.tuwien.domain.services.iml;
+
+/**
+ * Created by geoHeil on 07.03.16.
+ */
+public class JokeServiceImpl {
+}
