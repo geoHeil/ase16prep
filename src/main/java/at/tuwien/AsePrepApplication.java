ff1c03fa673e4565989b94c0d7c800e50e946916
geoHeil <e1225063@student.tuwien.ac.at>
2016-03-07 18:58:35 +0100

    some ideas for authentication + header handling

--- src/main/java/at/tuwien/AsePrepApplication.java
+++ src/main/java/at/tuwien/AsePrepApplication.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class AsePrepApplication implements CommandLineRunner {
@@ -25,7 +28,32 @@ public class AsePrepApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         LOG.info(apiUrl);
         RestTemplate restTemplate = new RestTemplate();
+        // it is possible to use String.class here to just read the plain JSON output
+        // checkout response wrappers for nested objects
+        // Link: http://www.baeldung.com/rest-template
+        // Authentication: http://stackoverflow.com/questions/21920268/basic-authentication-for-rest-api-using-spring-resttemplate
+
+        // Auth: 	public TestRestTemplate(String username, String password,
+        //http://stackoverflow.com/questions/10358345/making-authenticated-post-requests-with-spring-resttemplate-for-android
         Quote quote = restTemplate.getForObject(apiUrl, Quote.class);
         LOG.info(quote.toString());
+
+        //access to headers
+
+        //http://stackoverflow.com/questions/19238715/how-to-set-an-accept-header-on-spring-resttemplate-request
+        //http://stackoverflow.com/questions/16781680/http-get-with-headers-using-resttemplate
+        //http://stackoverflow.com/questions/6005709/how-do-i-read-the-response-header-from-resttemplate
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        ResponseEntity<String> result = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+        LOG.info(result.getHeaders().toString());
+        LOG.info("asdf");
+        LOG.info(result.getStatusCode().toString());
+//        LOG.info(result.getBody());
     }
 }
--- src/test/java/at/tuwien/AsePrepApplicationTests.java
+++ src/test/java/at/tuwien/AsePrepApplicationTests.java
@@ -1,18 +1,47 @@
 package at.tuwien;
 
+import at.tuwien.domain.Quote;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AsePrepApplication.class)
 @WebAppConfiguration
 public class AsePrepApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(AsePrepApplicationTests.class);
 
+    @Value("${spring.apiUrl}")
+    private String apiUrl;
+
+//    @Test
+//    public void testSuccessfulResponse() {
+//        LOG.info(apiUrl);
+//        RestTemplate restTemplate = new RestTemplate();
+//        // it is possible to use String.class here to just read the plain JSON output
+//        Quote quote = restTemplate.getForObject(apiUrl, Quote.class);
+//        LOG.info(quote.toString());
+//    }
+
+//    RestTemplate template = new TestRestTemplate();
+//
+//    @Test
+//    public void testRequest() throws Exception {
+//        HttpHeaders headers = template.getForEntity(apiUrl, String.class).getHeaders();
+//        System.out.println(headers.getLocation().toString());
+//        assertThat(headers.getLocation().toString(), containsString("fooj"));
+//    }
 }
