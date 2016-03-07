bb1142d3b8d04a368518ea3d98d12a23b51d3664
geoHeil <e1225063@student.tuwien.ac.at>
2016-03-07 17:43:11 +0100

    initial commit

---
+++ src/main/java/at/tuwien/AsePrepApplication.java
@@ -0,0 +1,12 @@
+package at.tuwien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsePrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsePrepApplication.class, args);
+	}
+}
